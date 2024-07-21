12917a9338c775eb4a2ebd277ab43eb0ea4919a1
papmatedaniel <90318564+papmatedaniel@users.noreply.github.com>
2024-07-21 21:17:38 +0000

    botujjitas

--- discordbot/main.py
+++ discordbot/main.py
@@ -5,7 +5,20 @@ bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print("Bot ready!")
+    print("Bot ready!") 
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello there, {ctx.author.mention}!")
+
+
+@bot.command()
+async def goodmorning(ctx):
+    await ctx.send(f"Good morning, {ctx.author.mention}!")
+
+with open("discordbot/token.txt") as f:
+    token = f.read()
 
 bot.run("")
 
